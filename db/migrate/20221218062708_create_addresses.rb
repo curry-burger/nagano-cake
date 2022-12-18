9e9fb7d87b08cf4326a5944b3492139c31999741
sushisyake <117505663+sushisyake@users.noreply.github.com>
2022-12-18 07:20:00 +0000

    after_migrate

--- db/migrate/20221218062708_create_addresses.rb
+++ db/migrate/20221218062708_create_addresses.rb
@@ -6,11 +6,6 @@ class CreateAddresses < ActiveRecord::Migration[6.1]
       t.string :post_code ,null: false
       t.string :address ,null: false
       t.string :name ,null: false
-      t.datetime :created_at ,null: false
-      t.datetime :updated_at ,null: false
-
-name
-
     end
   end
 end
