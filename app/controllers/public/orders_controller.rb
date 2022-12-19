d5f03953131bd45f7111e43b05eb1229d093968e
sushisyake <117505663+sushisyake@users.noreply.github.com>
2022-12-19 13:03:31 +0000

    add_order.rb

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -2,14 +2,16 @@ class Public::OrdersController < ApplicationController
 
   def new
     @order = Order.new
+    @customer=current_customer
+    @shipping_addresses=@customer
   end
 
   def confirm
-    
+
   end
 
   def create
-    
+
   end
 
   def complete
@@ -19,13 +21,13 @@ class Public::OrdersController < ApplicationController
   end
 
   def show
-    
+
   end
 
   private
 
   def order_params
-    
+
   end
 
 end
