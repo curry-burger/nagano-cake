f36c8d5e31e42d36df2918fc1b13a6101765d8fa
ta96ra <95111900+ta96ra@users.noreply.github.com>
2022-12-21 12:58:03 +0000

    Add default Items in seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -23,3 +23,63 @@ Genre.create(
     }
   ]
 )
+
+# 商品の初期値
+# Item.create!(
+#   [
+#     {
+#       genre_id: 1,
+#       name: 'チーズケーキ',
+#       description: '新商品！',
+#       price: 500,
+#       # item_image: File.open("./app/assets/images/cake1.jpg"),
+#       status: true
+#     },
+#     {
+#       genre_id: 2,
+#       name: 'クッキー',
+#       description: '新商品！',
+#       price: 100,
+#       # item_image: File.open("./app/assets/images/baked_goods1.jpg"),
+#       status: true
+#     },
+#     {
+#       genre_id: 3,
+#       name: 'チーズケーキ',
+#       description: '新商品！',
+#       price: 500,
+#       # item_image: File.open("./app/assets/images/cake2.jpg"),
+#       status: true
+#     },
+#     {
+#       genre_id: 4,
+#       name: 'なめらかプリン',
+#       description: '新商品！',
+#       price: 300,
+#       # item_image: File.open("./app/assets/images/candy1.jpg"),
+#       status: true
+#     },
+#     {
+#       genre_id: 5,
+#       name: 'キャンディ',
+#       description: '新商品！',
+#       price: 200,
+#       # item_image: File.open("./app/assets/images/pudding1.jpg"),
+#       status: true
+#     }
+#   ]
+# )
+
+5.times do |n|
+  item = Item.create!(
+    genre_id: n + 1,
+    name: 'チーズケーキ',
+    description: '新商品！',
+    price: 500,
+    status: true
+  )
+  item.item_image.attach(io: File.open(Rails.root.join("app/assets/images/cake1.jpg")),
+  filename: "cake1.jpg"
+
+  )
+end
