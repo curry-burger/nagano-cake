4ec3ec6599e6a03177c24278d6bb048a78b4ae8e
gaogaostar <115141870+gaogaostar@users.noreply.github.com>
2022-12-21 06:37:28 +0000

    Add create action

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -6,13 +6,15 @@ class Public::CartItemsController < ApplicationController
   end
 
   def update
+    cart_item = CartItem.find(params[:id])
+    cart_item.update(cart_item_params)
+    redirect_to cart_items_path(cart_item)
   end
 
   def destroy
     cart_item = CartItem.find(params[:id])
     cart_item.destroy
     redirect_to cart_items_path
-
   end
 
   def destroy_all
@@ -21,7 +23,15 @@ class Public::CartItemsController < ApplicationController
   end
 
   def create
-
+    item = CartItem.find_by(item_id: params[:cart_item][:item_id], customer_id: current_customer.id)
+    if item
+      item.update(quantity: item.quantity.to_i + params[:cart_item][:quantity].to_i)
+    else
+      cart_item = CartItem.new(cart_item_params)
+      cart_item.customer_id = current_customer.id
+      cart_item.save
+    end
+    redirect_to cart_items_path
   end
 
   private
