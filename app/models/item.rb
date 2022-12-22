27bf215109fa2d2cf443610da1d2c13d1140f4df
ta96ra <95111900+ta96ra@users.noreply.github.com>
2022-12-22 03:31:38 +0000

    repair item.rb

--- app/models/item.rb
+++ app/models/item.rb
@@ -11,7 +11,7 @@ class Item < ApplicationRecord
   def get_image
     unless item_image.attached?
       file_path = Rails.root.join('app/assets/images/no_image.jpg')
-      image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+      item_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
     end
     item_image
   end
