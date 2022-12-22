7a7f815629a9c87f6458b9c5a2b233bb0e897496
gaogaostar <115141870+gaogaostar@users.noreply.github.com>
2022-12-22 14:27:55 +0000

    [Fix]migrate

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,35 +6,35 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-# ジャンルの初期値
-# Genre.create(
-#   [
-#     {
-#       name: 'ケーキ'
-#     },
-#     {
-#       name: '焼き菓子'
-#     },
-#     {
-#       name: 'プリン'
-#     },
-#     {
-#       name: 'キャンディ'
-#     }
-#   ]
-# )
+ジャンルの初期値
+Genre.create(
+  [
+    {
+      name: 'ケーキ'
+    },
+    {
+      name: '焼き菓子'
+    },
+    {
+      name: 'プリン'
+    },
+    {
+      name: 'キャンディ'
+    }
+  ]
+)
 
 
-# 5.times do |n|
-#   item = Item.create!(
-#     genre_id: n + 1,
-#     name: 'チーズケーキ',
-#     description: '新商品！',
-#     price: 500,
-#     status: true
-#   )
-#   item.item_image.attach(io: File.open(Rails.root.join("app/assets/images/cake1.jpg")),
-#   filename: "cake1.jpg"
+5.times do |n|
+  item = Item.create!(
+    genre_id: n + 1,
+    name: 'チーズケーキ',
+    description: '新商品！',
+    price: 500,
+    status: true
+  )
+  item.item_image.attach(io: File.open(Rails.root.join("app/assets/images/cake1.jpg")),
+  filename: "cake1.jpg"
 
-#   )
-#end
+  )
+end
