c34c5880e8098b084934f679a0d71d375bf1d332
ta96ra <95111900+ta96ra@users.noreply.github.com>
2022-12-25 22:21:46 +0000

    dec26am1

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -23,6 +23,14 @@ class Admin::CustomersController < ApplicationController
       # render :edit
     end
   end
+  def exit
+    @customer = Customer.find(params[:id])
+    # is_deletedカラムをtrueに変更することにより削除フラグを立てる
+    @customer.update(is_deleted: true)
+    reset_session
+    flash[:notice] = "退会処理を実行いたしました"
+    redirect_to root_path
+  end
 
   private
 
